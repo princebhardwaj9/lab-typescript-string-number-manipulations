1d95ec4f9d054497a41b9245b04526113a25b7aa
princebhardwaj9 <princebhardwaj.110@gmail.com>
2020-04-21 23:37:07 +0530

    FSJ0320A021

--- started-code/main-service.ts
+++ started-code/main-service.ts
@@ -1,1 +1,33 @@
+interface StringManipulationService{
+    print(word:string): void;
+    printWithSpace(sentence:string) : void;
+    findVowel(str: string) : void;
+}
 
+class StringManipulations implements StringManipulationService{
+
+    print(word:string): void{
+        console.log(word);
+        console.log(word.toUpperCase());
+        console.log(word.toLowerCase());
+        console.log("Char At"+word.charAt(2));
+        console.log(word.concat(" Bhardwaj"));
+        console.log(word.slice(0,3));
+        console.log(word.length);
+    }
+
+    printWithSpace(sentence:string) : void{
+        console.log(sentence.split('').join(' '));
+    }
+
+    findVowel(str: string) : void{
+        var vcount:number=0;
+        let word=str.toString().toLowerCase();
+        for(var i=0;i<str.length;i++){
+            if(word.charAt(i)=="a" || word.charAt(i)=="e" || word.charAt(i)=="i" || word.charAt(i)=="o" || word.charAt(i)=="u"){
+                vcount=vcount+1;
+            }
+        }
+        console.log("Their are "+vcount+" vowels in "+str);
+    }
+}
--- started-code/main.ts
+++ started-code/main.ts
@@ -1,1 +1,32 @@
+interface NumberManipulationService{
+    findPrime(num: number) : void;
+    findMagic(num: number) : void;
+}
 
+class NumbersManipulations{
+    num:number;
+    findPrime(num: number) : void{
+        let n=Math.floor(num/2);
+        let nn:boolean=false;
+        for(var i=2;i<n;i++){
+            if(num%i==0){
+                nn=true;
+                break;
+            }
+            else{
+                nn=false;
+            }
+        }
+        if(nn){
+            console.log("It is not a Prime Number");
+        }
+        else{
+            console.log("Yes its a Prime Number");
+        }
+    }
+
+    constructor(num:number){
+        this.num=num;
+    }
+
+}
